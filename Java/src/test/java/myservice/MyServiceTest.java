e8d64a2baedc932f06306bb9f25bf6195b64b49d
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-08-21 13:33:07 +0200

    refactor to use a token instead of null

--- Java/src/test/java/myservice/MyServiceTest.java
+++ Java/src/test/java/myservice/MyServiceTest.java
@@ -6,27 +6,25 @@ import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.junit.Test;
 
-import sso.AuthenticationGateway;
-import sso.Request;
-import sso.Response;
-import sso.SingleSignOnRegistry;
+import sso.*;
 
 public class MyServiceTest {
 
     @Test
     public void invalidSSOTokenIsRejected() {
+        SSOToken token = new SSOToken();
         Mockery context = new Mockery();
         SingleSignOnRegistry ssoRegistry = context.mock(SingleSignOnRegistry.class);
         AuthenticationGateway authenticationGateway = context.mock(AuthenticationGateway.class);
         context.checking(new Expectations() {{
             ignoring(authenticationGateway);
 
-            oneOf(ssoRegistry).is_valid(null);
+            oneOf(ssoRegistry).is_valid(token);
             will(returnValue(false));
         }});
 
         MyService service = new MyService(ssoRegistry, authenticationGateway);
-        Response response = service.handleRequest(new Request("Foo", null));
+        Response response = service.handleRequest(new Request("Foo", token));
 
         context.assertIsSatisfied();
         assertNotEquals("hello Foo!", response.getText());
