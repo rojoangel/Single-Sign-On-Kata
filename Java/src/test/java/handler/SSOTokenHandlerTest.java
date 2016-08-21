b09facd3ff731f3ef84ac5bdee8a93cd7b574611
Angel Rojo Marijuan <arojomarijuan@magento.com>
2016-08-21 21:42:45 +0200

    sso token handler accepts valid token

--- Java/src/test/java/handler/SSOTokenHandlerTest.java
+++ Java/src/test/java/handler/SSOTokenHandlerTest.java
@@ -34,4 +34,26 @@ public class SSOTokenHandlerTest {
         context.assertIsSatisfied();
         assertNotEquals("hello Foo!", response.getText());
     }
+
+    @Test
+    public void validSSOTokenIsAccepted() throws Exception {
+        SSOToken token = new SSOToken();
+        Request request = new Request("World", token);
+
+        Mockery context = new Mockery();
+        Service service = context.mock(Service.class);
+        SingleSignOnRegistry registry = context.mock(SingleSignOnRegistry.class);
+
+        context.checking(new Expectations() {{
+            oneOf(service).handleRequest(request);
+
+            oneOf(registry).is_valid(token);
+            will(returnValue(true));
+        }});
+
+        SSOTokenHandler handler = new SSOTokenHandler(service, registry);
+        handler.handleRequest(request);
+
+        context.assertIsSatisfied();
+    }
 }
